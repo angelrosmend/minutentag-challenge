5d255140fd68594cab509fa8527eb5f8d7598572
root <angelrosmend@gmail.com>
2022-01-13 13:15:02 -0300

    (test): add productList and state

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import { TeamsList } from "./components/TeamsList";
 
 import './App.css';
 import { useState } from "react";
+import { Grocery } from "./components/Grocery";
+import { productsList } from "./utils/products";
 
 export default function App() {
-  const [focusable, setFocusable] = useState(false)
+  const [focusable, setFocusable] = useState(true)
+  const [products, setProducts] = useState(productsList)
   return (
     <div className="App">
       {/* Render here each component from the "components" directory */}
+      <h3>Grocery</h3>
+      <Grocery products={products} setProducts={setProducts}/>
       <h3>'Message' test</h3>
       <Message />
       <br />
---
+++ src/utils/products.js
@@ -0,0 +1,8 @@
+export const productsList = [
+    { name: "Oranges", votes: 0 }, 
+    { name: "Bananas", votes: 0 },
+    { name: "Apples", votes: 0 },
+    { name: "Berries", votes: 0 },
+    { name: "Grappes", votes: 0 },
+    { name: "Kiwies", votes: 0 },
+]
